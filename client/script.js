71970deb9fa8443c1ae606b22cb833a6b13165aa
vicen404 <vicentesanchez.alberto@gmail.com>
2023-01-30 14:01:10 +0100

    com-140110-013023

--- client/script.js
+++ client/script.js
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch(`https://${ip}:5000`, {
+    const response = await fetch(`https://openai-server-${ip}.traefik.me:5000`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
